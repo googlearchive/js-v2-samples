f14ae4926e46201199c1a1eff20c412bc57f181a
pammyla.fox <pammyla.fox@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2007-08-01 22:05:24 +0000

    Panoramio

--- trunk/panoramio/pano_layer.js
+++ trunk/panoramio/pano_layer.js
@@ -23,7 +23,7 @@ PanoramioLayerCallback.prototype.formPageUrl = function(photoId) {
 PanoramioLayerCallback.prototype.createMarker = function(photo, baseIcon) {
   var markerIcon = new GIcon(baseIcon);
   markerIcon.image = this.formImgUrl(photo.photo_id, "mini_square");
-  var marker = new GMarker(new GLatLng(photo.latitude, photo.longitude), {icon: markerIcon});
+  var marker = new GMarker(new GLatLng(photo.latitude, photo.longitude), {icon: markerIcon, title: photo.photo_title});
 
   if (photo.photo_title.length > 33) {
     photo.photo_title = photo.photo_title.substring(0, 33) + "&#8230;";

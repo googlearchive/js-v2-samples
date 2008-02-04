5b1863f898615a3eae23b71919f84848c7ec40be
gearylabs <gearylabs@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-02-04 17:05:33 +0000

    Use a single copy of miniresults.js

--- trunk/elections/2008/miniresults/miniresults.js
+++ trunk/elections/2008/miniresults/miniresults.js
@@ -1,26 +1,6 @@
-var contentBase = 'http://gmaps-samples.googlecode.com/svn/trunk/elections/2008/primary/states/super/';
-
-var prefs = new _IG_Prefs();
-var useTestData = prefs.getBool('testdata');
-var homepage = prefs.getBool('homepage');
-var which = homepage ? 'short' : 'long';
-
-var party = ( Math.random() < .5 ? 'dem' : 'gop' );
-
-setTimeout( function() {
-	_IG_FetchContent( 
-		contentBase + 'miniresults-' + which + '-' + party + '.html',
-		function( html ) {
-			document.getElementById('container').innerHTML = html;
-		},
-		{
-			refreshInterval:0
-		}
-	);
-}, 1 );
-
 document.write(
-	'<div id="container">',
-		'Loading&#8230',
-	'</div>'
+	'<script type="text/javascript" src="',
+		_IG_GetCachedUrl( 'http://gmaps-samples.googlecode.com/svn/trunk/elections/2008/primary/miniresults.js', { refreshInterval:120 } ),
+	'">',
+	'<\/script>'
 );

1a1b148a197342337923c795a75fc8f2da9693a7
gearylabs <gearylabs@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-02-05 14:08:23 +0000

    Tweak the Twitter updates

--- elections/2008/twitter/twitter-updater.rb
+++ elections/2008/twitter/twitter-updater.rb
@@ -2,6 +2,7 @@
 
 #puts 'starting'
 
+require 'banned-words'
 require 'secret'
 
 require 'hpricot'
@@ -9,7 +10,7 @@ require 'json'
 require 'net/http'
 require 'xmpp4r-simple'
 
-MAX_UPDATES = 100
+MAX_UPDATES = 30
 
 lastwrite = Time.now
 
@@ -18,9 +19,12 @@ updates = {}
 updatelist = []
 
 def writeupdates( updatelist )
-	File.open( 'tweets.js', 'w' ) do |f|
+	File.open( 'tweets/tweets.js', 'w' ) do |f|
 		f.puts updatelist.to_json
 	end
+	p 'Checking in updates'
+	`svn ci -m "Twitter update" tweets/tweets.js`
+	p 'Done checking in'
 end
 
 im = Jabber::Simple.new( Secret::USERNAME, Secret::PASSWORD )
@@ -28,43 +32,53 @@ im.deliver( 'twitter@twitter.com', 'on' )
 
 while true
 	im.received_messages { |msg|
-		if msg.type == :chat and msg.from == 'twitter@twitter.com' and not updates[msg.id]
+		if msg.type == :chat and msg.from == 'twitter@twitter.com' and not updates[msg.body]
 			body = msg.body
 			match = /^\s*\((.*)\):(.*)$/.match(body)
 			if match
 				username = match[1]
 				message = match[2]
-				user = users[username]
-				doc = Hpricot::XML(msg.to_s)
-				author = (doc/:author/:name).text
-				if not user
-					http = Net::HTTP.new( 'api.twittervision.com' )
-					headers, tvbody = http.get( "/user/current_status/#{username}.xml" )
-					if headers.code == '200'
-						tv = Hpricot::XML(tvbody)
-						loc = (tv/:location)
-						users[username] = {
-							'user' => username,
-							'name' => (tv/:name).text,
-							'image' => (tv/'profile-image-url').text,
-							'lat' => (loc/:latitude).text,
-							'lon' => (loc/:longitude).text,
-							'status' => 0
-						}
+				if Banned.banned(message)
+					p "Blocked: #{message}"
+				else
+					user = users[username]
+					doc = Hpricot::XML(msg.to_s)
+					author = (doc/:author/:name).text
+					if not user
+						http = Net::HTTP.new( 'api.twittervision.com' )
+						headers, tvbody = http.get( "/user/current_status/#{username}.xml" )
+						if headers.code == '200'
+							tv = Hpricot::XML(tvbody)
+							loc = (tv/:location)
+							lat = (loc/:latitude).text
+							lon = (loc/:longitude).text
+							if lat != '' and lon != ''
+								users[username] = {
+									'user' => username,
+									'author' => author,
+									'name' => (tv/:name).text,
+									'image' => (tv/'profile-image-url').text,
+									'lat' => (loc/:latitude).text,
+									'lon' => (loc/:longitude).text,
+									'where' => (tv/'current-location').text,
+									'status' => 0
+								}
+							end
+						end
 					end
-				end
-				user = users[username]
-				if user
-					update = {
-						'message' => message,
-						'time' => Time.xmlschema( (doc/:published).text ).to_i
-					}.merge( user )
-					updates[msg.id] = update
-					updatelist.push( update )
-					updatelist.delete_at(0) if updatelist.length > MAX_UPDATES
-					if Time.now - lastwrite > 300 or updatelist.length > 0
-						lastwrite = Time.now
-						writeupdates updatelist
+					user = users[username]
+					if user
+						update = {
+							'message' => message,
+							'time' => Time.xmlschema( (doc/:published).text ).to_i
+						}.merge( user )
+						updates[msg.body] = update
+						updatelist.push( update )
+						updatelist.delete_at(0) if updatelist.length > MAX_UPDATES
+						if Time.now - lastwrite > 300 # or updatelist.length > 1
+							lastwrite = Time.now
+							writeupdates updatelist
+						end
 					end
 				end
 			end

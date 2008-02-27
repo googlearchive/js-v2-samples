39ea0dc0b68288bc2e5aec461c60a0c8dccde041
gearylabs <gearylabs@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-02-27 18:07:02 +0000

    Refactor

--- elections/2008/twitter/twitter-updater.rb
+++ elections/2008/twitter/twitter-updater.rb
@@ -69,32 +69,9 @@ class Updater
 			p "Blocked: #{message}"
 			return
 		end
-		user = @users[username]
 		doc = Hpricot::XML(msg.to_s)
 		author = (doc/:author/:name).text
-		if not user
-			http = Net::HTTP.new( 'api.twittervision.com' )
-			headers, tvbody = http.get( "/user/current_status/#{username}.xml" )
-			if headers.code == '200'
-				tv = Hpricot::XML(tvbody)
-				loc = (tv/:location)
-				lat = (loc/:latitude).text
-				lon = (loc/:longitude).text
-				if lat != '' and lon != ''
-					@users[username] = {
-						'user' => username,
-						'author' => author,
-						'name' => (tv/:name).text,
-						'image' => (tv/'profile-image-url').text,
-						'lat' => (loc/:latitude).text,
-						'lon' => (loc/:longitude).text,
-						'where' => (tv/'current-location').text,
-						'status' => 0
-					}
-				end
-			end
-		end
-		user = @users[username]
+		user = getuser( username, author )
 		return if not user
 		update = {
 			'body' => msg.body,
@@ -113,6 +90,32 @@ class Updater
 			@lastwrite = Time.now
 			writeupdates
 		end
+	end
+	
+	def getuser( username, author )
+		if not @users[username]
+			http = Net::HTTP.new( 'api.twittervision.com' )
+			headers, body = http.get( "/user/current_status/#{username}.xml" )
+			if headers.code == '200'
+				tv = Hpricot::XML(body)
+				loc = (tv/:location)
+				lat = (loc/:latitude).text
+				lon = (loc/:longitude).text
+				if lat != '' and lon != ''
+					@users[username] = {
+						'user' => username,
+						'author' => author,
+						'name' => (tv/:name).text,
+						'image' => (tv/'profile-image-url').text,
+						'lat' => (loc/:latitude).text,
+						'lon' => (loc/:longitude).text,
+						'where' => (tv/'current-location').text,
+						'status' => 0
+					}
+				end
+			end
+		end
+		@users[username]
 	end
 	
 	def receive

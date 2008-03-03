760c222a23db74f257e2b07fb8563dc29f500c49
mano.marks <mano.marks@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-03-03 20:49:45 +0000

    Fixed so that it works with Python 2.3.x, and added better handling for the order argument.

--- articles-csvtokml/csvtokml.py
+++ articles-csvtokml/csvtokml.py
@@ -1,4 +1,4 @@
-﻿import geocoding_for_kml
+import geocoding_for_kml
 import csv
 import xml.dom.minidom
 import sys
@@ -9,21 +9,23 @@ def extractAddress(row):
   # ahead of time what the columns are that hold the address information.
   return '%s,%s,%s,%s,%s' % (row['Address1'], row['Address2'], row['City'], row['State'], row['Zip'])
 
-def createPlacemark(kmlDoc, row, columnOrder):
+def createPlacemark(kmlDoc, row, order):
   # This creates a <Placemark> element for a row of data.
   # A row is a dict.
   placemarkElement = kmlDoc.createElement('Placemark')
   extElement = kmlDoc.createElement('ExtendedData')
   placemarkElement.appendChild(extElement)
   
-  for key in columnOrder:
-    dataElement = kmlDoc.createElement('Data')
-    dataElement.setAttribute('name', key)
-    valueElement = kmlDoc.createElement('value')
-    dataElement.appendChild(valueElement)
-    valueText = kmlDoc.createTextNode(row[key])
-    valueElement.appendChild(valueText)
-    extElement.appendChild(dataElement)
+  # Loop through the columns and create a <Data> element for every field that has a value.
+  for key in order:
+    if row[key]:
+      dataElement = kmlDoc.createElement('Data')
+      dataElement.setAttribute('name', key)
+      valueElement = kmlDoc.createElement('value')
+      dataElement.appendChild(valueElement)
+      valueText = kmlDoc.createTextNode(row[key])
+      valueElement.appendChild(valueText)
+      extElement.appendChild(dataElement)
   
   pointElement = kmlDoc.createElement('Point')
   placemarkElement.appendChild(pointElement)
@@ -33,23 +35,21 @@ def createPlacemark(kmlDoc, row, columnOrder):
   pointElement.appendChild(coorElement)
   return placemarkElement
 
-def createKML(csvReader, fileName, order = None):
+def createKML(csvReader, fileName, order):
   # This constructs the KML document from the CSV file.
   kmlDoc = xml.dom.minidom.Document()
   
   kmlElement = kmlDoc.createElementNS('http://earth.google.com/kml/2.2', 'kml')
+  kmlElement.setAttribute('http://earth.google.com/kml/2.2','xmlns')
   kmlElement = kmlDoc.appendChild(kmlElement)
   documentElement = kmlDoc.createElement('Document')
   documentElement = kmlElement.appendChild(documentElement)
 
-  # If an argument was passed to the script, it splits the argument on a comma
-  # and uses the resulting list to specify an order for when columns get added.
-  columnOrder = None
-  if order:
-    columnOrder = order.split(',')
+  # Skip the header line.
+  csvReader.next()
+  
   for row in csvReader:
-    if columnOrder == None: columnOrder = row.keys()
-    placemarkElement = createPlacemark(kmlDoc, row, columnOrder)
+    placemarkElement = createPlacemark(kmlDoc, row, order)
     documentElement.appendChild(placemarkElement)
   kmlFile = open(fileName, 'w')
   kmlFile.write(kmlDoc.toprettyxml('  ', newl = '\n', encoding = 'utf-8'))
@@ -57,8 +57,15 @@ def createKML(csvReader, fileName, order = None):
 def main():
   # This reader opens up 'google-addresses.csv', which should be replaced with your own.
   # It creates a KML file called 'google.kml'.
-  csvreader = csv.DictReader(open('google-addresses.csv'))
-  kml = createKML(csvreader, 'google-addresses.kml', sys.argv[1])
+  
+  # If an argument was passed to the script, it splits the argument on a comma
+  # and uses the resulting list to specify an order for when columns get added.
+  # Otherwise, it defaults to the order used in the sample.
+  
+  if len(sys.argv) >1: order = sys.argv[1].split(',')
+  else: order = ['Office','Address1','Address2','Address3','City','State','Zip','Phone','Fax']
+  csvreader = csv.DictReader(open('google-addresses.csv'),order)
+  kml = createKML(csvreader, 'google-addresses.kml', order)
 
 if __name__ == '__main__':
   main()

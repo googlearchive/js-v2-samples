1710e7a8ba73226ab2033abd9f1423c099d522c5
pamela.fox <pamela.fox@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-03-05 06:41:26 +0000

    Adding html for elections map embed

--- trunk/elections/electionsmap_dynamicmarkers.js
+++ trunk/elections/electionsmap_dynamicmarkers.js
@@ -1705,7 +1705,7 @@ function log(bla) {
 function showPolys( state, party ) {
  var isState = false;
  if (state.name == "US") var isState = true;
- 
+ log(state); 
  log(isState);
  var tallies = state.votes && state.votes[party.name] || {};
  state.places.forEach( function( place ) {

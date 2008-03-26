04ad1621f98f9b2d1d59cda3d979def68c54a84b
pamela.fox <pamela.fox@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-03-26 00:35:55 +0000

    Adding code for geocoding article

---
+++ trunk/articles-phpsqlgeocode/phpsqlgeocode_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Pan Africa Market', '1521 1st Ave, Seattle, WA', '0.0', '0.0', 'restaurant');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Buddha Thai & Bar', '2222 2nd Ave, Seattle, WA', '0.0', '0.0', 'bar');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('The Melting Pot', '14 Mercer St, Seattle, WA', '0.0', '0.0', 'restaurant');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Ipanema Grill', '1225 1st Ave, Seattle, WA', '0.0', '0.0', 'restaurant');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Sake House', '2230 1st Ave, Seattle, WA', '0.0', '0.0', 'bar');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Crab Pot', '1301 Alaskan Way, Seattle, WA', '0.0', '0.0', 'restaurant');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Mama\'s Mexican Kitchen', '2234 2nd Ave, Seattle, WA', '0.0', '0.0', 'bar');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Wingdome', '1416 E Olive Way, Seattle, WA', '0.0', '0.0', 'bar');
+INSERT INTO `markers` (`name`, `address`, `lat`, `lng`, `type`) VALUES ('Piroshky Piroshky', '1908 Pike pl, Seattle, WA', '0.0', '0.0', 'restaurant');
---
+++ trunk/articles-phpsqlgeocode/phpsqlgeocode_xml.php
@@ -0,0 +1,74 @@
+<?php
+require("phpsqlajax_dbinfo.php");
+
+define("MAPS_HOST", "maps.google.com");
+define("KEY", "abcdefg");
+
+// Opens a connection to a MySQL server
+$connection = mysql_connect("localhost", $username, $password);
+if (!$connection) {
+  die("Not connected : " . mysql_error());
+}
+
+// Set the active MySQL database
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die("Can\'t use db : " . mysql_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM markers2 WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die("Invalid query: " . mysql_error());
+}
+
+// Initialize delay in geocode speed
+$delay = 0;
+$base_url = "http://" . MAPS_HOST . "/maps/geo?output=xml" . "&key=" . KEY;
+
+// Iterate through the rows, geocoding each address
+while ($row = @mysql_fetch_assoc($result)) {
+  $geocode_pending = true;
+
+  while ($geocode_pending) {
+    $address = $row["address"];
+    $id = $row["id"];
+    $request_url = $base_url . "&q=" . urlencode($address);
+    $xml = simplexml_load_file($request_url) or die("url not loading");
+
+    $status = $xml->Response->Status->code;
+    if (strcmp($status, "200") == 0) {
+      // Successful geocode
+      $geocode_pending = false;
+      $coordinates = $xml->Response->Placemark->Point->coordinates;
+      $coordinatesSplit = split(",", $coordinates);
+      // Format: Longitude, Latitude, Altitude
+      $lat = $coordinatesSplit[1];
+      $lng = $coordinatesSplit[0];
+
+      $query = sprintf("UPDATE markers2 " .
+             " SET lat = '%s', lng = '%s' " .
+             " WHERE id = '%s' LIMIT 1;",
+             mysql_real_escape_string($lat),
+             mysql_real_escape_string($lng),
+             mysql_real_escape_string($id));
+      $update_result = mysql_query($query);
+      if (!$update_result) {
+        die("Invalid query: " . mysql_error());
+      }
+    } else if (strcmp($status, "620") == 0) {
+      // sent geocodes too fast
+      $delay += 100000;
+    } else {
+      // failure to geocode
+      $geocode_pending = false;
+      echo "Address " . $address . " failed to geocoded. ";
+      echo "Received status " . $status . "
+\n";
+    }
+    usleep($delay);
+  }
+}
+?>
+

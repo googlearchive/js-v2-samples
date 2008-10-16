44459d0615bb81e67d957a5e7bc8a04a301b6b35
pammyla.fox <pammyla.fox@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2008-10-16 02:15:47 +0000

    Fixing basic example

--- slides-interactive/slides/basic.js
+++ slides-interactive/slides/basic.js
@@ -1,8 +1,7 @@
+<html>
 <head>
-  <script 
-   type="text/javascript"
-   href="http://maps.google.com/maps?
-         file=api&v=2&key=ABQ...c">
+  <script type="text/javascript" src="http://maps.google.com/maps?file=api&v=2&key=ABQ...c"></script>
+  <script>
   function createMap() {
     var map = new GMap2(document.getElementById("map"));
     map.setCenter(new GLatLng(37.44, -122.14), 14);
@@ -10,3 +9,6 @@
   </script>
 </head>
 <body onload="createMap()" onunload="GUnload()">
+ <div id="map" style="width:100%;height:50%"></div>
+</body>
+</html>

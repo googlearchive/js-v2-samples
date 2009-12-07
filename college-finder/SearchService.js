1403a3aaf59621ab2f8cbb4105f7ca67fbcbc726
holden.matt <holden.matt@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2009-12-07 22:07:49 +0000

    Added a college-finder demo built using the Maps Data API

---
+++ college-finder/SearchService.js
@@ -0,0 +1,301 @@
+/*
+ * SearchService.js
+ * This class is responsible for making search requests to the Maps Data API
+ * and returning the results.
+ *
+ *
+ * Copyright (c) 2009, Google Inc. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Thor Mitchell
+ * @author Matt Holden
+ */
+
+if (! window.google) {
+  window.google = {};
+}
+if (! google.code) {
+  google.code = {};
+}
+google.code.mapsearch = {};
+google.code.mapsearch.COMMON_LOADER_URI_ = 'http://www.google.com/jsapi';
+google.code.mapsearch.MAPS_FEEDS_URI_ = 'http://maps.google.com/maps/feeds';
+
+/**
+ * Class that provides the search functionality and parses the results
+ * into an easy to use array. Modeled on the Maps API v3 Geocoder class.
+ * @constructor
+ * @param application_name {String} A string identifier for your application.
+ */
+google.code.mapsearch.SearchService = function(application_name) {
+  // Check that required dependencies are already loaded
+  if (! google.maps || ! google.maps.Geocoder) {
+    throw new Error('Maps API v3 must be loaded prior to creating SearchService.');
+  }
+  else if (! google.gdata || ! google.gdata.maps) {
+    throw new Error('Maps Data API must be loaded prior to creating SearchService.');
+  }
+  
+  this.parser_ = new DOMParser();
+  this.geocoder_ = new google.maps.Geocoder();
+  this.mapsService_ = new google.gdata.maps.MapsService(application_name);
+  
+  this.defaultMaxResults_ = 10;
+  this.radius_re_ = /^(\d+)[ ]?(m|km|miles)$/
+}
+
+/**
+ * Take a SearchRequest object literal, specifying a spatial and/or attribute
+ * search on a single map, and return the search results as an Array.
+ *
+ * request {Object} SearchRequest literal containing the search options.
+ *   Valid options are:
+ *   <ul>
+ *   <li>latLng {google.maps.LatLng}: A location around which to search.  When a
+ *       radius is specified, the search returns placemarks within the radius of
+ *       this location.  The sortBy option can also indicate search results should
+ *       be sorted by distance to this location.  If an address and latLng are both
+ *       supplied, the latLng takes precedence.
+ *   <li>address {string}: A text address that is geocoded to a latLng prior to searching.
+ *   <li>bounds {google.maps.LatLngBounds}: A geographical bounding box to search
+ *       within. If this option is supplied it takes precedence over address and
+ *       latLng for results filtering, but results can still be sorted by distance
+ *       to a location.
+ *   <li>radius {string}: A distance specified as '<number> <units>' representing
+ *       the distance from the specified address or latLng to search. Valid
+ *       values for <units> are 'm', 'km', or 'miles'. This parameter only applies
+ *       if an address or latLng option was supplied.
+ *   <li>mapId {string}: The map ID of the map over which to search.  This ID can be
+ *       found in two ways: in the Maps Data API map feed or in the 'Link' URL for the
+ *       map in the Google My Maps interface at http://maps.google.com/maps/mm.
+ *   <li>filters {Array string}: An array of attribute filter expressions of the form
+ *       '[name: value]' (for example, '[has_wireless: true]).
+ *   <li>maxResults {number}: Maximum numbers of results to return (defaults to 10).
+ *   <li>sortBy {string}: The only option currently supported is sortBy 'distance',
+ *       which sorts locations by their proximity to the address or latLng location.
+ *   <li>startIndex {string}: Index in the full list of results of the first
+ *        result to include in the response. Used for paging results.
+ *   </ul>
+ * @param callback {Function} Reference to function that will be called with an array of
+ *   results when the search returns.
+ */
+google.code.mapsearch.SearchService.prototype.search = function(request, callback) {
+  if (request.address) {
+    this.geocoder_.geocode({ address: request.address }, this.handleGeocodeResponse_(request, callback));
+  } else {
+    this.parseRequest_(request, callback);
+  }
+}
+
+/**
+ * Extract the lat/lng from the geocoder response before parsing the search request.
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.handleGeocodeResponse_ = function(request, callback) {
+  var me = this;
+  return function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      request.latLng = results[0].geometry.location;
+      console.log("Successfully geocoded " + request.address + ": " + request.latLng);
+      me.parseRequest_(request, callback);
+    } else {
+      // TODO: improve error handling.
+      alert("Sorry, we couldn't find a location matching " + request.address);
+    }
+  }
+}
+
+/**
+ * Parse the arguments of a search request object to create the corresponding
+ * feature feed search URL for the Maps Data API.  Request the search results
+ * using the given callback.
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.parseRequest_ = function(request, callback) {
+  var me = this;
+  
+  var featureFeedUrl = google.code.mapsearch.MAPS_FEEDS_URI_ + '/features';
+  var params = [];
+  
+  if (request.mapId) {
+    featureFeedUrl += '/' + request.mapId + '/snippet?';
+  } else {
+    throw new Error('mapId is required');
+  }
+  
+  /* A latLng option will override an address option if both are provided */
+  if (request.latLng) {
+    params.push('lat=' + this.roundCoord_(request.latLng.lat()));
+    params.push('lng=' + this.roundCoord_(request.latLng.lng()));
+  }
+  
+  /* This precedes checking for bounds because it is not permitted
+   * to set a radius on a bounds filter.
+   */
+  if (request.radius) {
+    var radius_units = this.radius_re_.exec(request.radius);
+    if (radius_units == null) {
+      throw new Error("Search radius must be a number followed by a unit (m, km, or miles).");
+    }
+    
+    var radius = radius_units[1];
+    var units = radius_units[2];
+    
+    // Convert radius to meters.
+    if (units == 'km') {
+      radius *= 1000;
+    } else if (units == 'miles') {
+      radius *= 1609.344;
+    }
+    params.push('radius=' + radius);
+  }
+  
+  if (request.bounds) {
+    var sw = request.bounds.getSouthWest()
+    var ne = request.bounds.getNorthEast();
+    params.push('box=' +
+                this.roundCoord_(sw.lng()) + ',' +
+                this.roundCoord_(sw.lat()) + ',' +
+                this.roundCoord_(ne.lng()) + ',' +
+                this.roundCoord_(ne.lat()));
+  }
+
+  // Attribute filters can be provided either with or without enclosing square brackets.
+  if (request.filters) {
+    var mq = '';
+    for (var i = 0; i < request.filters.length; i++) {
+      var filter = request.filters[i];
+      if (filter.indexOf('[') == 0 && filter.lastIndexOf(']') == (filter.length - 1)) {
+        mq += filter;
+      } else {
+        mq += '[' + filter + ']';
+      }
+    }
+    params.push('mq=' + mq);
+  }
+  
+  if (request.sortBy) {
+    params.push('sortBy=' + request.sortBy);
+  }
+  
+  if (request.maxResults) {
+    params.push('max-results=' + request.maxResults);
+  } else {
+    params.push('max-results=' + this.defaultMaxResults_);
+  }
+  
+  if (request.startIndex) {
+    params.push('start-index=' + request.maxResults);
+  }
+  
+  featureFeedUrl += params.join('&');
+  
+  this.mapsService_.getFeatureFeed(featureFeedUrl, function(feedRoot) {
+    me.returnResults_(feedRoot.feed.getEntries(), callback);
+  });
+}
+
+/**
+ * Converts an array of google.gdata.maps.FeatureEntry objects into an
+ * easy to use Array of typed results and returns it to the application via the
+ * callback given to SearchService().
+ *
+ * @param entries {Array google.gdata.maps.FeatureEntry} The results of the Maps GData API search.
+ * @param callback {Function} Reference to function that will be called with the parsed results.
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.returnResults_ = function(entries, callback) {
+  var results = new Array(entries.length);
+  for (var i in entries) {
+    var kml = this.parseKml_(entries[i].getContent().getText());
+    var placemark = kml.getElementsByTagNameNS('http://www.opengis.net/kml/2.2', 'Placemark').item(0);
+    
+    results[i] = {};
+    results[i]['id']        = entries[i].getId().getValue();
+    results[i]['published'] = entries[i].getPublished().getValue().getDate();
+    results[i]['updated']   = entries[i].getUpdated().getValue().getDate();
+    results[i]['title']     = entries[i].getTitle().getText();
+    results[i]['name'] = this.getNodeContent_(placemark, 'name');
+    // TODO: when the /full search projection is added, we will have descriptions.
+    // results[i]['description'] = this.getNodeContent(placemark, 'description');
+    results[i]['marker'] = this.getMarker_(placemark);
+    
+    results[i]['properties'] = {};
+    var properties = entries[i].getCustomProperties();
+    for (var j = 0; j < properties.length; j++) {
+      var prop = properties[j];
+      var name = prop.getName();
+      var type = prop.getType();
+      var unit = prop.getUnit();
+      var value = prop.getValue();
+      if (! results[i]['properties'][name]) {
+        results[i]['properties'][name] = [];
+      }
+      results[i]['properties'][name].push(value);
+    }
+  }
+  callback(results);
+}
+
+/**
+ * Parse the KML returned by the Maps Data API for a feature.
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.parseKml_ = function(kmlString) {
+  // The Data API returns a KML <Placemark>, so we must wrap it before parsing it.
+  var string = '<?xml version="1.0"?><kml xmlns:kml="http://www.opengis.net/kml/2.2">' + kmlString + '</kml>';
+  return this.parser_.parseFromString(string, 'text/xml');
+}
+
+/**
+ * Return the value of the first tag of the given type within a KML node.
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.getNodeContent_ = function(node, tag) {
+  return node.getElementsByTagNameNS('http://www.opengis.net/kml/2.2', tag).item(0).childNodes[0].nodeValue;
+}
+
+/**
+ * Construct a Google Maps API LatLng object from a Google Data API search result.
+ *
+ * @param entry  {google.gdata.maps.FeatureEntry} A Maps Data API feature search result.
+ * @return {google.maps.LatLng}
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.getMarker_ = function(placemark) {
+  var coords = this.getNodeContent_(placemark, 'coordinates').split(',');
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(coords[1], coords[0]),
+    title: name,
+  });
+
+  var iconNode = placemark.getElementsByTagNameNS('http://www.opengis.net/kml/2.2', 'Icon').item(0);
+  if (iconNode) {
+    var iconHref = this.getNodeContent_(iconNode, 'href');
+    marker.setIcon(iconHref);
+  }
+
+  return marker;
+}
+
+/**
+ * Round a decimal coordinate to 6 decimal places.
+ *
+ * @param coord {Number}
+ * @returns {Number}
+ * @private
+ */
+google.code.mapsearch.SearchService.prototype.roundCoord_ = function(coord) {
+  return (Math.round(coord * 1000000) / 1000000)
+}
---
+++ college-finder/data/csv_to_kml.py
@@ -0,0 +1,184 @@
+"""
+Currently, the Maps Data API only supports importing data as KML, so this script
+converts the CSV college data to a KML file.  You can upload a KML file to My Maps
+by going to http://maps.google.com/maps/mm, clicking 'Create new map' and then 'Import',
+after which you can use the Maps Data API to access the map.
+
+When we allow direct CSV uploads to the Maps Data API in the near future, converting
+CSV to KML will no longer be necessary.
+"""
+
+
+import os
+import sys
+import csv
+import time
+import urllib
+import xml.dom.minidom
+
+USAGE = 'Usage: python %s <csv_input_filename> <kml_output_filename>' % os.path.basename(__file__)
+
+MAPS_API_KEY = 'ABQIAAAAAXRHBzvcYZkkEqRaBgXApxRVxhLeBhMD54-1xBmv7u9irWOHFRS9wQwnbtAF-EiES-FsXYAfX_SMHw'
+GEOCODER_URL = 'http://maps.google.com/maps/geo?q=%s&output=csv&key=' + MAPS_API_KEY
+
+
+def geocode(address):
+    """Geocodes the given address using the Google Maps API HTTP geocoder,
+    and returns the longitude,latitude as a string."""
+    url = GEOCODER_URL % urllib.quote(address)
+    (status, accuracy, lat, lng) = urllib.urlopen(url).read().split(',')
+    
+    if status == '200':
+        return '%s,%s' % (lng, lat)
+    else:
+        print '  ERROR: Geocoding failed for address %s (status %s)' % (address, status)
+        return None
+
+
+def create_placemark_element(kml_doc, name, description, coordinates, extended_data={}):
+    """Create a KML Point placemark using a given name, description, and lng/lat coordinates.
+    If any text key/value pairs are specified as extended_data,
+    they are added as KML ExtendedData tags.  The Placemark is returned as a DOM element
+    for the given kml_doc XML DOM Document."""
+    
+    # Create <Placemark>
+    placemark_element = kml_doc.createElement('Placemark')
+    
+    # Add <name>
+    name_element = kml_doc.createElement('name')
+    name_element.appendChild(kml_doc.createTextNode(name))
+    placemark_element.appendChild(name_element)
+    
+    # Add <description>
+    description_element = kml_doc.createElement('description')
+    description_element.appendChild(kml_doc.createTextNode(description))
+    placemark_element.appendChild(description_element)
+    
+    # Add <ExtendedData>
+    if len(extended_data) > 0:
+        extended_data_element = kml_doc.createElement('ExtendedData')
+        placemark_element.appendChild(extended_data_element)
+        for name in sorted(extended_data):
+            value = extended_data[name]
+            data_element = kml_doc.createElement('Data')
+            data_element.setAttribute('name', name)
+            value_element = kml_doc.createElement('value')
+            value_element.appendChild(kml_doc.createTextNode(value))
+            data_element.appendChild(value_element)
+            extended_data_element.appendChild(data_element)
+    
+    # Add <Point>
+    point_element = kml_doc.createElement('Point')
+    coordinates_element = kml_doc.createElement("coordinates")
+    coordinates_element.appendChild(kml_doc.createTextNode(coordinates))
+    point_element.appendChild(coordinates_element)
+    placemark_element.appendChild(point_element)
+    
+    return placemark_element
+
+
+def create_kml_document(document_name, csv_input_filename, kml_output_filename, modify_row_dict=None):
+    """Create a KML document from a CSV file.  Each CSV row is read into a dictionary, which
+    can be optionally modified by a given modify_row_dict function before being converted to
+    a KML placemark."""
+    
+    # Open the input/output files
+    if os.path.isfile(csv_input_filename):
+        csv_input_file  = open(csv_input_filename, 'r')
+        kml_output_file = open(kml_output_filename, 'w')
+    else:
+        sys.exit("Invalid file name: %s" % csv_input_filename)
+    
+    # Create the KML document and <kml> element.
+    kml_doc = xml.dom.minidom.Document()
+    kml_element = kml_doc.createElement('kml')
+    kml_element.setAttribute('xmlns', 'http://www.opengis.net/kml/2.2')
+    kml_element = kml_doc.appendChild(kml_element)
+    
+    # Add <Document>
+    document_element = kml_doc.createElement('Document')
+    document_element = kml_element.appendChild(document_element)
+    
+    # Add document <name>
+    name_element = kml_doc.createElement('name')
+    name_element.appendChild(kml_doc.createTextNode(document_name))
+    document_element.appendChild(name_element)
+    
+    # Read each CSV row into a dictionary, optionally modify the dictionary,
+    # and use it to create a Placemark that is appended to the KML Document element.
+    reader = csv.DictReader(csv_input_file)
+    for line_num, row in enumerate(reader):
+        # If a modify_row_dict function was given, apply it to the dictionary for each row.
+        if modify_row_dict:
+            modify_row_dict(row)
+        
+        # Check that this row has all required fields.
+        required_fields = ['name', 'description', 'address']
+        for field in required_fields:
+            if not field in row or not row[field]:
+                raise Exception('Line %d is missing a required field: %s' % (line_num + 1, field))
+        
+        # Add any non-required fields as extended data.
+        extended_data = {}
+        for field in row:
+            if not field in required_fields:
+                extended_data[field] = row[field]
+        
+        # Delay for a moment to avoid exceeding geocoder quota.
+        time.sleep(0.05)
+        
+        name = row['name']
+        description = row['description']
+        coordinates = geocode(row['address'])
+        if coordinates != None:
+            print 'Adding Placemark: %s' % name
+            placemark_element = create_placemark_element(kml_doc, name, description, coordinates, extended_data)
+            document_element.appendChild(placemark_element)
+    
+    # Write the KML document to the output file, formatted as pretty XML.
+    csv_input_file.close()
+    kml_output_file.write(kml_doc.toprettyxml('  '))
+    kml_output_file.close()
+
+
+if __name__ == '__main__':
+    # Get the input/output filenames from the command line.
+    if len(sys.argv) == 3:
+        csv_input_filename  = sys.argv[1]
+        kml_output_filename = sys.argv[2]
+    else:
+        sys.exit(USAGE)
+    
+    def modify_row_dict(row):
+        """Modify a single row of CSV college data before it's converted to KML."""
+        
+        # Use 3 Type values: Public, Private, or Unknown
+        school_type = row['Type']
+        if 'Public' in school_type:
+            school_type = 'Public'
+        elif 'Private' in school_type:
+            school_type = 'Private'
+        else:
+            raise 's'
+            school_type = 'Unknown'
+        
+        # Group counts of student population into buckets
+        size = row['Size']
+        if size == '' or not size >= 0:
+            size = 'Unknown'
+        elif size < 100:
+            size = '0-100 students'
+        elif size < 1000:
+            size = '101-1000 students'
+        elif size < 10000:
+            size = '1001-10000 students'
+        else:
+            size = '10000+ students'
+        
+        row['description'] = '%s <br /> <b>Type:</b> %s <br /> <b>Size:</b> %s' % (row['address'], school_type, size)
+        row['Website'] = row['Website'].lower()
+        row['Type'] = school_type
+        row['Size'] = size
+    
+    
+    create_kml_document("4-Year U.S. Colleges", csv_input_filename, kml_output_filename, modify_row_dict)

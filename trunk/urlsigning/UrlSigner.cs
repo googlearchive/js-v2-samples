49b2b904969fc8bd503411c81969f76d768efbcd
manshreck <manshreck@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2010-02-03 04:18:16 +0000

    Updated C# example to properly assemble URL rather than interpet as String

--- trunk/urlsigning/UrlSigner.cs
+++ trunk/urlsigning/UrlSigner.cs
@@ -5,12 +5,11 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 
-namespace SignUrl
-{
-    public struct GoogleSignedUrl
-    {
-        public static string Create(Uri url)
-        {
+namespace SignUrl {
+
+    public struct GoogleSignedUrl {
+
+        public static Uri Sign(Uri url) {
             ASCIIEncoding encoding = new ASCIIEncoding();
 
             // key provided by Google
@@ -22,7 +21,7 @@ namespace SignUrl
             string encodedPathAndQuery = url.LocalPath + url.Query;
 
             // get the bytes of the encoded url
-            byte[] encodedPathAndQueryBytes = encoding.GetBytes(url.LocalPath + url.Query);
+            byte[] encodedPathAndQueryBytes = encoding.GetBytes(encodedPathAndQuery);
 
             // compute the hash
             HMACSHA1 algorithm = new HMACSHA1(privateKeyBytes);
@@ -30,17 +29,39 @@ namespace SignUrl
 
             // convert the bytes to string and make url-safe by replacing '+' and '/' characters
             string signature = Convert.ToBase64String(hash).Replace("+", "-").Replace("/", "_");
+            
+             // Add the signature to the existing URI.
+            UriBuilder new_uri = new UriBuilder(url);
+            if (String.IsNullOrEmpty(new_uri.Query)) {
+                new_uri.Query = "signature=" + signature;
+            } else {
+                // Note: (MS Bug) The Query property returns the query string
+                // with the starting '?' but expect to be set without.
+                new_uri.Query = new_uri.Query.TrimStart(new char[] {'?'}) + "&signature=" + signature;
+            }
 
-            // return the reassembled the url
-            return url.Scheme + "://" + url.Host + encodedPathAndQuery + "&signature=" + signature;
+            return new_uri.Uri;    
         }
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(GoogleSignedUrl.Create(new Uri(args[0])));
+    class Program {
+
+        static void Main(string[] args) {
+
+           try {
+                // Convert the string into a URL.
+                Uri url = new Uri(args[0]);
+
+                // Create a new URL with the signature added.
+                Uri new_url = GoogleSignedUrl.Sign(url);
+
+                // Outputs the URL as a string.
+                // Note: (MS Bug) The 'toString()' method returns the Uri unescaped.
+                // AbsoluteUri and OriginalUri return the unescaped string.
+                Console.WriteLine(new_url.OriginalString);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

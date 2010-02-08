964659173be4fb8f6d87d3241c96ce71792e1e9c
manshreck <manshreck@bd4feb60-9d29-0410-bcad-4118a2cb9d5c>
2010-02-08 04:12:52 +0000

    Updated C# example to have similar input processing to Java and Python examples.

--- urlsigning/UrlSigner.cs
+++ urlsigning/UrlSigner.cs
@@ -9,19 +9,15 @@ namespace SignUrl {
 
   public struct GoogleSignedUrl {
 
-    public static Uri Sign(Uri url) {
+    public static string Sign(string url, string keyString) {
       ASCIIEncoding encoding = new ASCIIEncoding();
 
-      // key provided by Google
-      const string PRIVATE_KEY = "YOUR_PRIVATE_KEY";
-
       // converting key to bytes will throw an exception, need to replace '-' and '_' characters first.
-      string usablePrivateKey = PRIVATE_KEY.Replace("-", "+").Replace("_", "/");
+      string usablePrivateKey = keyString.Replace("-", "+").Replace("_", "/");
       byte[] privateKeyBytes = Convert.FromBase64String(usablePrivateKey);
-      string encodedPathAndQuery = url.LocalPath + url.Query;
 
-      // get the bytes of the encoded url
-      byte[] encodedPathAndQueryBytes = encoding.GetBytes(encodedPathAndQuery);
+      Uri uri = new Uri(url);
+      byte[] encodedPathAndQueryBytes = encoding.GetBytes(uri.LocalPath + uri.Query);
 
       // compute the hash
       HMACSHA1 algorithm = new HMACSHA1(privateKeyBytes);
@@ -29,43 +25,43 @@ namespace SignUrl {
 
       // convert the bytes to string and make url-safe by replacing '+' and '/' characters
       string signature = Convert.ToBase64String(hash).Replace("+", "-").Replace("/", "_");
-         
+            
       // Add the signature to the existing URI.
-      UriBuilder new_uri = new UriBuilder(url);
-      if (String.IsNullOrEmpty(new_uri.Query)) {
-         new_uri.Query = "signature=" + signature;
-      } else {
-         // Note: (MS Bug) The Query property returns the query string
-         // with the starting '?' but expect to be set without.
-         new_uri.Query = new_uri.Query.TrimStart(new char[] {'?'}) + "&signature=" + signature;
-      }
-      
-      return new_uri.Uri;    
+      return url +"&signature=" + signature; 
     }
   }
 
   class Program {
 
-    static void Main(string[] args) {
+    static void Main() {
+    
+      // Note: Generally, you should store your private key someplace safe
+      // and read them into your code
 
-      try {
-         
-        Uri url = new Uri("YOUR_URL_TO_SIGN");;
-        // Convert the string into a URL.
-        if (args.Length > 0) {
-          url = new Uri(args[0]);
-        }
-
-        // Create a new URL with the signature added.
-        Uri new_url = GoogleSignedUrl.Sign(url);
-
-        // Outputs the URL as a string.
-        // Note: (MS Bug) The 'toString()' method returns the Uri unescaped.
-        // AbsoluteUri and OriginalUri return the unescaped string.
-        Console.WriteLine(new_url.OriginalString);
-      } catch (Exception ex) {
-        Console.WriteLine(ex.Message);
+      const string keyString = "YOUR_PRIVATE_KEY";
+  
+      // The URL shown in these examples is a static URL which should already
+      // be URL-encoded. In practice, you will likely have code
+      // which assembles your URL from user or web service input
+      // and plugs those values into its parameters.
+      const  string urlString = "YOUR_URL_TO_SIGN";
+      
+      string inputUrl = null;
+      string inputKey = null;
+    
+      Console.WriteLine("Enter the URL (must be URL-encoded) to sign: ");
+      inputUrl = Console.ReadLine();
+      if (inputUrl.Length == 0) {
+        inputUrl = urlString;
+      }     
+    
+      Console.WriteLine("Enter the Private key to sign the URL: ");
+      inputKey = Console.ReadLine();
+      if (inputKey.Length == 0) {
+        inputKey = keyString;
       }
+      
+      Console.WriteLine(GoogleSignedUrl.Sign(inputUrl,inputKey));
     }
   }
 }

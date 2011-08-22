4d3f1039f6a56f090483ca47e346579e75191673
kbrisbin <kbrisbin@google.com>
2011-08-22 21:36:45 +0000

    adding demo hash

--- fusiontables/script/script.js
+++ fusiontables/script/script.js
@@ -1,26 +1,27 @@
-//adds style and header to sample pages
+var demo = window.location.hash;
+if (demo != "#demo") {
+  style = document.createElement("link");
+  style.type = "text/css";
+  style.rel = "stylesheet";
+  style.href = "css/style.css";
 
-style = document.createElement("link");
-style.type = "text/css";
-style.rel = "stylesheet";
-style.href = "css/style.css";
+  head = document.getElementsByTagName("head")[0];
+  head.appendChild(style);
 
-head = document.getElementsByTagName("head")[0];
-head.appendChild(style);
+  header = document.createElement("div");
+  header.id = "header";
 
-header = document.createElement("div");
-header.id = "header";
+  para = document.createElement("p");
 
-para = document.createElement("p");
+  tag = document.createElement("a");
+  tag.href = "http://code.google.com/apis/fusiontables/docs/sample_code.html";
+  tag.innerHTML = "&lt; Back to Fusion Tables samples";
 
-tag = document.createElement("a");
-tag.href = "http://code.google.com/apis/fusiontables/docs/sample_code.html";
-tag.innerHTML = "&lt; Back to Fusion Tables samples";
+  para.appendChild(tag);
+  header.appendChild(para);
 
-para.appendChild(tag);
-header.appendChild(para);
+  document.body.insertBefore(header, document.body.childNodes[0]);
 
-document.body.insertBefore(header, document.body.childNodes[0]);
-
-document.getElementById('code').innerHTML = 
-  "<p>The Code:</p>" + "<pre>" + document.getElementById('script').innerHTML.replace(/</g, "&lt;") + "</pre>";
+  document.getElementById('code').innerHTML = 
+    "<p>The Code:</p>" + "<pre>" + document.getElementById('script').innerHTML.replace(/</g, "&lt;") + "</pre>";
+}
